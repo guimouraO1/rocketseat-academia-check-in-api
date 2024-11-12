044351c1f02f955948b82eb6024b668a485f74c9
Guilherme de Moura Oliveira <guilherme@microbit.com.br>
2024-11-12 13:04:54 -0300

    O check-in só pode ser validado até 20 minutos após ser criado

--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -40,4 +40,21 @@ describe("Validate Check-in use case", () => {
             })
         ).rejects.toBeInstanceOf(ResourceNotFoundError);
     });
+
+    it("should not be able to validate the check-in after 20 minutes f its creation", async () => {
+        vi.setSystemTime(new Date(2024, 0, 1, 13, 40));
+
+        const createdCheckIn = await checkInsRepository.create({
+            gym_id: "gym-01",
+            user_id: "user-01"
+        });
+
+        const twentyOneMinutesInMs = 1000 * 60 * 21;
+
+        vi.advanceTimersByTime(twentyOneMinutesInMs);
+
+        await expect(() =>
+            sut.execute({ checkInId: createdCheckIn.id })
+        ).rejects.toBeInstanceOf(Error);
+    });
 });
--- src/use-cases/validate-check-in.ts
+++ src/use-cases/validate-check-in.ts
@@ -1,6 +1,7 @@
 import { ResourceNotFoundError } from "./errors/resource-not-found-error";
 import { CheckIn } from "@prisma/client";
 import { CheckInsRepository } from "@/repositories/check-ins-repository";
+import dayjs from "dayjs";
 
 interface ValidateCheckInUseCaseRequest {
     checkInId: string;
@@ -20,6 +21,15 @@ export class ValidateCheckInUseCase {
 
         if (!checkIn) {
             throw new ResourceNotFoundError();
+        }
+
+        const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+            checkIn.created_at,
+            "minutes"
+        );
+
+        if (distanceInMinutesFromCheckInCreation > 20) {
+            throw new Error();
         }
 
         checkIn.validated_at = new Date();
