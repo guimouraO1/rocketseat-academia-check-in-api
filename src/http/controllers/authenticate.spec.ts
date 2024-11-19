22f8dad3394e67a9fbc0d503ec99a00bb9147854
Guilherme de Moura Oliveira <guilherme@microbit.com.br>
2024-11-19 17:09:29 -0300

    Ajustando postgres database e começando testes e2e

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,3 +1,29 @@
-import { test } from "vitest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import request from "supertest";
+import { app } from "@/app";
 
-test("ok", () => {});
+describe("Authenticate controller", () => {
+    beforeAll(async () => {
+        await app.ready();
+    });
+
+    afterAll(async () => {
+        await app.close();
+    });
+
+    it("shoud be able to authenticate", async () => {
+        await request(app.server).post("/users").send({
+            name: "Jhon Dow",
+            email: "jhondoe@example.com",
+            password: "123456"
+        });
+
+        const response = await request(app.server).post("/sessions").send({
+            email: "jhondoe@example.com",
+            password: "123456"
+        });
+
+        expect(response.statusCode).toEqual(200);
+        expect(response.body).toEqual({ token: expect.any(String) });
+    });
+});
--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,23 @@
-import { test } from "vitest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import request from "supertest";
+import { app } from "@/app";
 
-test("ok", () => {});
+describe("Register controller", () => {
+    beforeAll(async () => {
+        await app.ready();
+    });
+
+    afterAll(async () => {
+        await app.close();
+    });
+
+    it("shoud be able to register", async () => {
+        const response = await request(app.server).post("/users").send({
+            name: "Jhon Dow",
+            email: "jhondoe@example.com",
+            password: "123456"
+        });
+
+        expect(response.statusCode).toEqual(201);
+    });
+});
